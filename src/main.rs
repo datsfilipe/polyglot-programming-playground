57a01c4b9ee5c4147299e667c7ed89ad9ef79ed1
datsfilipe <datsfilipe.foss@proton.me>
2023-06-15 14:56:41 -0300

    feat: rust handling errors

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,18 @@
-fn main() {
+fn error_me(throw: bool) -> Result<(), String> {
+    if throw {
+        Err("I'm an error".to_string())
+    } else {
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), String> {
+    let value = error_me(false)?;
+    // the above "?" do the same as the below
+    let _value_2 = match error_me(false) {
+        Err(e) => return Err(e),
+        Ok(v) => v,
+    };
+
+    return Ok(value);
 }
